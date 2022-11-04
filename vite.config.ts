1a560e74775f442043017f321a48da6409e1cf27
mindulle <mindullestudio@gmail.com>
2022-11-04 22:30:36 +0900

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// https://vitejs-kr.github.io/guide/static-deploy.html#github-pages
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [vue()]
+
 })
